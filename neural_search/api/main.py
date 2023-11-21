ce6767f05ebb813cf9ca7f2567f471d4bc01efa9
girinman <lsjg9909@naver.colm>
2023-11-21 11:09:16 +0900

    feat: neural search engine

---
+++ neural_search/api/main.py
@@ -0,0 +1,79 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from api.chroma import (
+    create_collection,
+    check_collection,
+    delete_collection,
+    index_documents,
+    search_documents,
+)
+from api.models import (
+    CreateRequest,
+    DeleteRequest,
+    IndexingRequest,
+)
+
+app = FastAPI()
+
+@app.get("/health")
+async def health() -> JSONResponse:
+    return "OK"
+
+@app.post("/create")
+async def create(request: CreateRequest) -> JSONResponse:
+    # 컬렉션 생성 로직
+    try:
+        collection = create_collection(request.collection_name, request.metadata)
+        return JSONResponse(status_code=200, content={"status": "success"})
+    except:
+        return JSONResponse(status_code=500, content={"status": "failure"})
+
+@app.get("/check/{collection_name}")
+async def check(collection_name: str = "test") -> JSONResponse:
+    # ChromeDB에서 컬렉션 존재 여부를 확인하는 로직
+    try:
+        collection = check_collection(collection_name)
+        return JSONResponse(
+            status_code=200,
+            content={
+                "status": "success",
+                "result":{
+                    "name": collection.name,
+                    "id": collection.id.hex,
+                    "metadata": collection.metadata,
+                }
+            }
+        )
+    except:
+        return JSONResponse(status_code=500, content={"status": "failure"})
+
+@app.delete("/delete")
+async def delete(request: DeleteRequest) -> JSONResponse:
+    # 컬렉션 제거 로직
+    try:
+        delete_collection(request.collection_name)
+        return JSONResponse(status_code=200, content={"status": "success"})
+    except:
+        return JSONResponse(status_code=500, content={"status": "failure"})
+
+@app.post("/indexing")
+async def indexing(request: IndexingRequest) -> JSONResponse:
+    # 문서 저장 로직
+    try:
+        index_documents(request.collection_name, request.data)
+        return JSONResponse(status_code=200, content={"status": "success"})
+    except:
+        return JSONResponse(status_code=500, content={"status": "failure"})
+
+@app.get("/search/{collection_name}")
+async def search(collection_name: str = "text",
+                 query: str = "Hi, there!",
+                 top_k: int = 2) -> JSONResponse:
+    try:
+        result = search_documents(collection_name, query, top_k)
+        return JSONResponse(status_code=200, content={
+            "status": "success",
+            "result": result
+        })
+    except:
+        return JSONResponse(status_code=500, content={"status": "failure"})
---
+++ neural_search/api/models.py
@@ -0,0 +1,20 @@
+from typing import List, Dict
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+# JSON 요청 본문을 위한 Pydantic 모델 정의
+class CreateRequest(BaseModel):
+    collection_name: str = "test"
+    metadata: Dict = None
+    
+class DeleteRequest(BaseModel):
+    collection_name: str = "test"
+
+# 개별 문서와 메타데이터를 포함하는 모델
+class Document(BaseModel):
+    document: str = "Hello, World!"
+    metadata: Dict = None
+
+class IndexingRequest(BaseModel):
+    collection_name: str = "test"
+    data: List[Document]
