aa4754973c61a8cabad45e87ae0d1769c5fd7b67
girinman <lsjg9909@naver.colm>
2023-12-02 16:26:08 +0900

    feat: hide llm api url

--- streamlit_app/src/llm.py
+++ streamlit_app/src/llm.py
@@ -1,22 +1,28 @@
 import requests
 import json
+import os
+from typing import List
 
-url = "http://localhost:40101/generate_stream"
+from dotenv import load_dotenv
+load_dotenv()
 
-def get_rag_query(query, documents):
+url = os.environ("LLM_API_URL")
+
+def get_rag_query(query: str, documents: List[str]):
     context = "\n\n".join(documents)
     query = f"""{context}
     
-Use the above information to answer the following query below.
-Query: {query}"""
+Based on the information given above, answer the questions below:
+{query}"""
     return query
 
 def get_generation_prompt(messages, search_results=[]):
     chat_prompt = ""
     documents = [item["document"] for item in search_results]
     for i, message in enumerate(messages):
-        if i > 0 and i < len(messages) - 3:
-            continue
+        # 대화 턴이 너무 긴 경우 최근 메시지만 사용
+        # if i > 0 and i < len(messages) - 3:
+        #     continue
         role = message["role"]
         content = message["content"]
         
@@ -35,8 +41,9 @@ def generate(prompt: str, max_new_tokens: int = 1024):
     data = {
         "inputs": prompt,
         "parameters": {
-            #"max_new_tokens": max_new_tokens,
-            "repetition_penalty": 1.05,
+            "max_new_tokens": max_new_tokens,
+            #"truncate": True,
+            "repetition_penalty": 1.03,
             "details": False,
             "decoder_input_details": False,
             "stop": ["</s>"]
